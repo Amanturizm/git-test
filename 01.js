bac5e86c5968cb7c5d5bf61d6bb7c568bd0363ec
Amantur Momunjanov <amanturmomunjanov12580@gmail.com>
2023-05-12 18:02:27 +0600

    finish add all

--- 01.js
+++ 01.js
@@ -1,1 +1,14 @@
 "use strict";
+const board = [];
+for (let i = 1; i < 8; i++) {
+    for (let j = 1; j < 8; j++) {
+        if ((i + j) % 2 === 0) {
+            board.push('  ');
+        }
+        else {
+            board.push('\u2588\u2588');
+        }
+    }
+    board.push('\n');
+}
+console.log(board.join(''));
