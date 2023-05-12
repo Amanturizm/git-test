bb093ab64f8c8464275bce49fc8fc6cfca087d05
Amantur Momunjanov <amanturmomunjanov12580@gmail.com>
2023-05-12 16:57:22 +0600

    add board logic

---
+++ 01.ts
@@ -0,0 +1,14 @@
+const board: string[] = [];
+
+for (let i: number = 1; i < 8; i++) {
+    for (let j: number = 1; j < 8; j++) {
+        if ((i + j) % 2 === 0) {
+            board.push('  ');
+        } else {
+            board.push('\u2588\u2588')
+        }
+    }
+    board.push('\n');
+}
+
+console.log(board.join(''));
